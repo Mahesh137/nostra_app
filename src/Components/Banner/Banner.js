83cd91ddc955869a867fa98091448b38339f7043
Mahesh_137 <mahesh.kumar@flynava.com>
2020-11-14 17:55:30 +0530

    third commit

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import Carousel from 'react-bootstrap/Carousel';
-import { BannerItem } from "./BannerItem";
 import { Link } from "react-router-dom";
 import "./Banner.css";
 
@@ -27,46 +26,30 @@ const Banner = (props) => {
 
     console.log("this", props.bannerlist)
     return (
-        <div className="bannercontainer">
-            {/* <div className="banner">
-                {state.map((a, i) => {
-                    return <BannerItem key={i} imagesrc={a.img_url} redirect={a.redirect} route={a.route}></BannerItem>
-                })}
-            </div> */}
-            <div className="banner">
-                <div className="banneritem">
-                    <Carousel>
-                        {state.map((a, i) => {
-                            return (
-                                (a.redirect) ?
-                                    <Carousel.Item interval={5000} key={i}>
-                                        <div className="banneritem">
-                                            <Link to={a.route || "/"}>
-                                                <img
-                                                    className="banner"
-                                                    src={a.img_url}
-                                                    alt="First slide"
-                                                />
-                                            </Link>
-                                        </div>
-                                    </Carousel.Item>
-                                    :
-                                    <Carousel.Item interval={5000} key={i}>
-                                        <div className="banneritem">
-
-                                            <img
-                                                className="banner"
-                                                src={a.img_url}
-                                                alt="First slide"
-                                            />
-                                        </div>
-                                    </Carousel.Item>
-                            )
-                        })}
-                    </Carousel>
-                </div>
-            </div>
-        </div>
+        <Carousel>
+            {state.map((a, i) => {
+                return (
+                    (a.redirect) ?
+                        <Carousel.Item interval={5000} key={i}>
+                            <Link to={a.route || "/"}>
+                                <img
+                                    className="d-block w-100"
+                                    src={a.img_url}
+                                    alt="First slide"
+                                />
+                            </Link>
+                        </Carousel.Item>
+                        :
+                        <Carousel.Item interval={5000} key={i}>
+                            <img
+                                className="d-block w-100"
+                                src={a.img_url}
+                                alt="First slide"
+                            />
+                        </Carousel.Item>
+                )
+            })}
+        </Carousel>
     )
 }
 
--- src/Components/Banner/BannerItem/index.js
+++
@@ -1,2 +0,0 @@
-
-export { BannerItem } from "./BannerItem";
